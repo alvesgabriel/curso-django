0c110914e0a9fd71a3a166ed95915b3a242c0302
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-09-19 11:25:03 -0300

    :sparkles: Created landing page
    close #82

--- pypro/urls.py
+++ pypro/urls.py
@@ -17,11 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 from pypro import settings
-from pypro.base.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home),
+    path("", include("pypro.base.urls")),
 ]
 
 if settings.DEBUG:
