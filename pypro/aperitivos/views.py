9eb9d88b8ee07192706ea365df0273ea715ce2d1
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-09-20 19:51:29 -0300

    :sparkles: Added template context
    close #96

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def videos(request, slug):
-    return render(request, "aperitivos/video.html")
+    video = {"titulo": "Vídeo Aperitivo: Motivação", "vimeo_id": 459799222}
+    return render(request, "aperitivos/video.html", context={"video": video})
