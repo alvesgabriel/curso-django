86b559d4ef28d2202d776a7fdaa5a7d1326fdf91
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-09-20 20:05:00 -0300

    :sparkles: Created page to second video using slug
    close #98

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def videos(request, slug):
-    video = {"titulo": "Vídeo Aperitivo: Motivação", "vimeo_id": 459799222}
+    videos = {
+        "motivacao": {"titulo": "Vídeo Aperitivo: Motivação", "vimeo_id": 459799222},
+        "instalacao-windows": {"titulo": "Instalação Windows", "vimeo_id": 251497668},
+    }
+    video = videos[slug]
     return render(request, "aperitivos/video.html", context={"video": video})
