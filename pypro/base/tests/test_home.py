8c86c99ae802ed161ecd05504f323fe959f49e29
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-09-21 08:13:29 -0300

    :sparkles: Extracted footer with template include
    close #106

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -22,5 +22,9 @@ def test_home_link(resp):
     assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
 
 
+def test_email_link(resp):
+    assert_contains(resp, 'href="mailto:ramalho@python.pro.br"')
+
+
 def test_home_body(resp):
     assert_contains(resp, "Curso de Python com Luciano Ramalho")
