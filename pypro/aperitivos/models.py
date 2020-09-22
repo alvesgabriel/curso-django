47afa36911cc6fa5e64979ddb32557a0eb9e75bf
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-09-22 20:30:41 -0300

    :sparkles: Added videos to admin area

--- pypro/aperitivos/models.py
+++ pypro/aperitivos/models.py
@@ -3,10 +3,13 @@ from django.urls import reverse
 
 
 class Video(models.Model):
-    slug = models.CharField(max_length=30)
     titulo = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=30)
     vimeo_id = models.CharField(max_length=30)
     creation = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
         return reverse("aperitivos:video", args=(self.slug,))
+
+    def __str__(self):
+        return f"Video: {self.titulo}"
