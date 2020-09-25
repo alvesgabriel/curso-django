f6e13b13f2d1b8587bd191beca4caad1a75f3f3d
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-09-25 07:57:34 -0300

    :sparkles: Added aulas list in the page detalhe

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -4,4 +4,5 @@ from pypro.modulos import facade
 
 def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
-    return render(request, "modulos/detalhe.html", context={"modulo": modulo})
+    aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
+    return render(request, "modulos/detalhe.html", context={"modulo": modulo, "aulas": aulas})
