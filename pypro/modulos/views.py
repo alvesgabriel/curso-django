b295caea02f59860f81ce9c51029f52f45e17439
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-09-25 08:12:07 -0300

    :sparkles: Added aula link in the page modulo detalhe

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -6,3 +6,7 @@ def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
     return render(request, "modulos/detalhe.html", context={"modulo": modulo, "aulas": aulas})
+
+
+def aula(resp, slug):
+    pass
