eca289f62179cb9f9be1b007ac1b10cd8b680331
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-09-25 07:57:34 -0300

    :sparkles: Added aulas list in the page detalhe

--- pypro/modulos/tests/test_detalhe_modulos.py
+++ pypro/modulos/tests/test_detalhe_modulos.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from model_bakery import baker
 
 from pypro.django_assertions import assert_contains
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Modulo, Aula
 
 
 @pytest.fixture
@@ -12,17 +12,27 @@ def modulo(db):
 
 
 @pytest.fixture
-def resp(client, modulo):
+def aulas(modulo):
+    return baker.make(Aula, 3, modulo=modulo)
+
+
+@pytest.fixture
+def resp(client, modulo, aulas):
     return client.get(reverse("modulos:detalhe", kwargs={"slug": modulo.slug}))
 
 
-def test_titulo_modulo(resp, modulo):
+def test_titulo(resp, modulo):
     assert_contains(resp, modulo.titulo)
 
 
-def test_publico_modulo(resp, modulo):
+def test_publico(resp, modulo):
     assert_contains(resp, modulo.publico)
 
 
-def test_descricao_modulo(resp, modulo):
+def test_descricao(resp, modulo):
     assert_contains(resp, modulo.descricao)
+
+
+def test_aulas_titulos(resp, aulas):
+    for aula in aulas:
+        assert_contains(resp, aula.titulo)
