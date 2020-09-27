05415f278fd75c98dee3affd332ec037f16af0d4
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-09-27 18:21:29 -0300

    :sparkles: Create page with modulos index
    close #141

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from pypro.modulos import views
 
 app_name = "modulos"
 urlpatterns = [
+    path("", views.indice, name="indice"),
     path("<slug:slug>/", views.detalhe, name="detalhe"),
     path("aula/<slug:slug>/", views.aula, name="aula"),
 ]
