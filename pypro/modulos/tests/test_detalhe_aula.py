342d713cf1dc1547aa7d185c8e3e88337d54ab3e
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-10-05 07:44:26 -0300

    :sparkles: Made page aulas only access to user logged (#162)

--- pypro/modulos/tests/test_detalhe_aula.py
+++ pypro/modulos/tests/test_detalhe_aula.py
@@ -16,8 +16,8 @@ def aula(modulo):
 
 
 @pytest.fixture
-def resp(client, aula: Aula):
-    return client.get(reverse("modulos:aula", kwargs={"slug": aula.slug}))
+def resp(client_usuario_logado, aula: Aula):
+    return client_usuario_logado.get(reverse("modulos:aula", kwargs={"slug": aula.slug}))
 
 
 def test_titulo(resp, aula: Aula):
@@ -32,3 +32,13 @@ def test_modulo_breadcrumb(resp, modulo: Modulo):
     assert_contains(
         resp, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{modulo.titulo}</a></li>'
     )
+
+
+@pytest.fixture
+def resp_sem_usuario(client, aula: Aula):
+    return client.get(reverse("modulos:aula", kwargs={"slug": aula.slug}))
+
+
+def test_usuario_nao_logado_redirect(resp_sem_usuario):
+    assert resp_sem_usuario.status_code == 302
+    assert resp_sem_usuario.url.startswith(reverse("login"))
